1f8c986c03952d0016472a544f2e7cffa9d03535
Denis Logvinenko <logvinenko@libra.de>
2023-11-20 16:03:28 +0100

    Added keybinding for showing active window list; fixed script for randomly selecting and setting wallpaper from folder

--- scripts/set_wallpaper_from_folder.sh
+++ scripts/set_wallpaper_from_folder.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 if [ -d $wallpaper_base_dir ]; then
-    nitrogen --set $(find $1 -type f -name "*.png" | shuf -n 1)
+	nitrogen --set-scaled $(find $1 -type f -name "*.png" | shuf -n 1)
+fi
